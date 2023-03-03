6be6ecdaf783b268b340ff1f471704558f4d22ac
denjayx <denyw602@gmail.com>
2023-03-03 22:03:45 +0700

    add pantun

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const hiddenContent = document.querySelector(".content");
+const Contents = document.querySelector(".content");
 const button = document.getElementById("button");
+const image = document.createElement("img");
+const text = document.getElementById("text");
+const h4 = document.createElement("h4");
+const BreakLine = document.createElement("br");
+const pantunBtn = document.createElement("button");
+pantunBtn.className = "btnOk";
+const callLink = document.createElement("a");
+callLink.className = "btnOk";
 
 function welcome() {
   alert("Tekan Ok Kalo Kamu Lagi Sedih");
-  hiddenContent.removeAttribute("hidden");
+  Contents.removeAttribute("hidden");
 }
 function giveUp() {
-  const text = document.getElementById("text");
   text.innerText = "Semangat!✌️";
-  const image = document.createElement("img");
   image.setAttribute("src", "semangat.jpg");
   image.className = "imageSemangat";
-  const h4 = document.createElement("h4");
-  h4.innerText = "Lama Nggak Ngobrol Nih 🐊";
-  hiddenContent.appendChild(image);
-  hiddenContent.appendChild(h4);
+  h4.innerHTML = `Bunga disiram takkan layu <br/> Selalu berbunga tanpa jemu <br/> Jangan takut kehilanganku <br/> Aku takkan pergi dari sisimu`;
+  pantunBtn.innerText = "Masih Sedih Ya?😔, Klik Lagi Dong";
+  Contents.appendChild(image);
+  Contents.appendChild(h4);
+  Contents.appendChild(pantunBtn);
+}
+function masihKurang() {
+  text.innerText =
+    "Mmm Kalo Pake Pantun Masih Kurang Semangat, Coba Langsung Call Aja Deh 👉👈";
+  image.setAttribute("src", "kucing.jpg");
+  image.className = "imageSemangat";
+  pantunBtn.style.display = "none";
+  callLink.setAttribute("href", "tapiboong.html");
+  callLink.innerText = "Call Ajahh 🫰";
+  Contents.appendChild(image);
+  Contents.appendChild(BreakLine);
+  Contents.appendChild(callLink);
+  h4.style.display = "none";
 }
 
 button.addEventListener("click", () => {
@@ -23,3 +43,4 @@ button.addEventListener("click", () => {
 
 document.body.onload = welcome;
 button.onclick = giveUp;
+pantunBtn.onclick = masihKurang;
