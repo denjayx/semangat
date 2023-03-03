9f54270c82dc0581fb0803ac47f419ab2fd20964
denjayx <denyw602@gmail.com>
2023-03-03 20:50:20 +0700

    styling

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const hiddenContent = document.querySelector(".content");
 const button = document.getElementById("button");
 
 function welcome() {
-  alert("Tekan Oke Jika Harimu Kurang Berwarna");
+  alert("Tekan Ok Kalo Kamu Lagi Sedih");
   hiddenContent.removeAttribute("hidden");
 }
 function giveUp() {
   const text = document.getElementById("text");
   text.innerText = "Semangat!✌️";
-  const image = document.createElement('img');
-  image.setAttribute('src', 'semangat.jpg');
-  image.setAttribute('width', '200');
+  const image = document.createElement("img");
+  image.setAttribute("src", "semangat.jpg");
+  image.className = "imageSemangat";
   const h4 = document.createElement("h4");
   h4.innerText = "Lama Nggak Ngobrol Nih 🐊";
   hiddenContent.appendChild(image);
   hiddenContent.appendChild(h4);
 }
 
-button.addEventListener('click',()=>{
-    button.style.display = 'none';
-})
+button.addEventListener("click", () => {
+  button.style.display = "none";
+});
 
 document.body.onload = welcome;
 button.onclick = giveUp;
